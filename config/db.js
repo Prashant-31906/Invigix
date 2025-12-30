4f36b8cfb4d20938bbe9371310058aaa71b5c48e
prashant-31906 <soni85299@gmail.com>
2025-12-30 19:52:48 +0530

    added username

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config(); // Local testing ke liye
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,       // Render se aayega
-  user: process.env.DB_USER,       // Render se aayega
+  user: process.env.DB_USERNAME,       // Render se aayega
   password: process.env.DB_PASSWORD, // Render se aayega
   database: process.env.DB_NAME,   // Render se aayega
   port: process.env.DB_PORT || 4000, // TiDB ka default port 4000 hota hai
